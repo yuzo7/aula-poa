95d64df4c5a88869d4da373a271348dee6050f3a
yuzo7 <112702463+yuzo7@users.noreply.github.com>
2023-08-09 11:45:42 -0300

    Add files via upload

---
+++ jogador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula
+{
+    class jogador : Program
+    {
+        string nome;
+        int idade;
+        int drible;
+        int velocidade;
+        int chute;
+        int forca;
+
+        public void correr()
+        {
+            int aceleraçao = força + velocidade;
+        }
+
+        public int chutar()
+        {
+            int potencia +forca * chute;
+            return potenciachute;
+        }
+
+        public void tocar()
+        {
+            double toque 1 / 3 chute + 1 / 3 força;
+        }
+
+        public void interceptor()
+        {
+            double potenciacarrinho velocidade*2 + 1/2 força;
+        }
+
+        public void driblar()
+        {
+            int eficienciadrible drible + (velocidade) * 2;
+        }
+
+    }
+}
